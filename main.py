add0251731e035eda6ff5e4d4e276cad57fe2368
EricContino <evcontino5107@gmail.com>
2025-06-09 13:10:02 -0400

    added call function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.genai import types
 from dotenv import load_dotenv
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -51,7 +51,12 @@ def generate_content(client, messages, verbose):
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("response doesn't exist")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        return function_call_result.parts[0].function_response.response
 
 
 if __name__ == "__main__":
